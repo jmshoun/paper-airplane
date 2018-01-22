17af10a458af93b57c7908bb475b6dfab59d7748
Mark Shoun <mark.shoun@gmail.com>
2018-01-21 23:47:19 -0500

    Suppress histogram warning message

--- utils/plot.R
+++ utils/plot.R
@@ -9,7 +9,7 @@ plot_method_means <- function(fit, model.name, log.scale=FALSE) {
   
   base.plot <- ggplot(plot.data) +
     aes(x=distance) +
-    geom_histogram(aes(y=..count../sum(..count..))) + 
+    geom_histogram(aes(y=..count../sum(..count..)), bins=30) + 
     geom_vline(xintercept=0) +
     scale_y_continuous(labels=scales::percent) +
     labs(x="Difference in Distance (meters)", y="Proportion of Simulations", 
